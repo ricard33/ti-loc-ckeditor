cb7a014dadc6d23b21cbe6b05eb214fc7ce1d127
Cédric RICARD <ricard33@gmail.com>
2023-03-19 22:14:24 -0400

    Build v0.0.5 - CKEditor5 upgraded to v36

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -1,11 +1,12 @@
 /**
- * @license Copyright (c) 2014-2020, CKSource - Frederico Knabben. All rights reserved.
+ * @license Copyright (c) 2014-2023, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor.js';
+import DecoupledEditor from '@ckeditor/ckeditor5-editor-decoupled/src/decouplededitor.js';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment.js';
-import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat.js';
+import AutoImage from '@ckeditor/ckeditor5-image/src/autoimage.js';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote.js';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold.js';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials.js';
@@ -27,7 +28,7 @@ import Link from '@ckeditor/ckeditor5-link/src/link.js';
 import List from '@ckeditor/ckeditor5-list/src/list.js';
 import PageBreak from '@ckeditor/ckeditor5-page-break/src/pagebreak.js';
 import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph.js';
-import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js';
 import RemoveFormat from '@ckeditor/ckeditor5-remove-format/src/removeformat.js';
 import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js';
 import SpecialCharacters from '@ckeditor/ckeditor5-special-characters/src/specialcharacters.js';
@@ -39,17 +40,18 @@ import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar.js';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation.js';
 import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline.js';
-import InsertSignature from "../plugins/insert-signature";
-import CustomFigureAttributes from "../plugins/custom-figure-attributes";
+// import InsertSignature from "../plugins/insert-signature";
+// import CustomFigureAttributes from "../plugins/custom-figure-attributes";
 
-class Editor extends ClassicEditor {}
+// class Editor extends ClassicEditor {}
+class Editor extends DecoupledEditor {}
 
 // Plugins to include in the build.
 Editor.builtinPlugins = [
 	Alignment,
-	AutoImage,
 	Autoformat,
-	BlockQuote,
+	AutoImage,
+	// BlockQuote,
 	Bold,
 	Essentials,
 	FontColor,
@@ -83,8 +85,8 @@ Editor.builtinPlugins = [
 	TextTransformation,
 	Underline,
   // --- custom plugins
-  InsertSignature,
-  CustomFigureAttributes
+  // InsertSignature,
+  // CustomFigureAttributes
 ];
 
 export default Editor;
